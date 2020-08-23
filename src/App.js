bf17f3f741e73dfb4d0ec1e38377e1a9c2f0e7d0
Conner Schiller <conner.schiller@gmail.com>
2020-08-22 17:52:48 -0700

    set up API call in api-index.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React, {Component} from 'react';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import {fetchData} from './api';
 
 class App extends Component {
+
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
     render() {
         return(
-            <div>
-                <h1>Hello World</h1>
+            <div className={styles.container}>
                 <Cards />
-                <Chart />
                 <CountryPicker />
+                <Chart />
             </div>
         )
     }
