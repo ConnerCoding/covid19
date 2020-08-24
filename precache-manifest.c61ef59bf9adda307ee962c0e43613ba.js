f20096c47ef8a6a1e987747158e61eb13e629d3f
Conner Schiller <conner.schiller@gmail.com>
2020-08-24 11:33:12 -0700

    Updates

---
+++ precache-manifest.c61ef59bf9adda307ee962c0e43613ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826c3034bf533f2dbb05a38d2186206b",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "05a47b57bc90192d26d3",
+    "url": "/covid19/static/css/main.304677c2.chunk.css"
+  },
+  {
+    "revision": "d1840544118cf89bd023",
+    "url": "/covid19/static/js/2.31430d66.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19/static/js/2.31430d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a47b57bc90192d26d3",
+    "url": "/covid19/static/js/main.a8cb8dd8.chunk.js"
+  },
+  {
+    "revision": "5d8f462d54057bd2b894",
+    "url": "/covid19/static/js/runtime-main.5288846b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
